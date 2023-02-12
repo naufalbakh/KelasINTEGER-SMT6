119312051fdfe32ad69f562536c0a29bcdb991af
Naufal Bakhtiar <76803127+naufalbakh@users.noreply.github.com>
2023-02-12 21:52:02 +0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@
 
 
 
-function openSUBJ(evt, cityName) {
+function openSUBJ(evt, subjName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -17,10 +17,29 @@ function openSUBJ(evt, cityName) {
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
 
+    }
+    document.getElementById(subjName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+}
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// function searchBar() {
+//     var tablinks;
+//     tablinks = document.getElementsByClassName("tablinks");
+//     if (tablinks == document.getElementById("WAJIB").click()){
+//         document.getElementById("wajibBar").style.display = "none";
+//     } else {
+// 		document.getElementById("optBar").style.display = "none";
+//     }
+// }
